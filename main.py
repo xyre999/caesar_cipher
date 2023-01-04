d1790f9f1f6160177957779c6f41b13da5cb9b0e
unknown <alaeddineguida@gmail.com>
2023-01-04 17:52:56 +0100

    Update main file

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower_alpha = "abcdefghijklmnopqrstuvwxyz"
 
-def main(user_msg):
-    
+def encrypte(user_msg):
     
     while True:
         try:
@@ -45,6 +44,58 @@ def main(user_msg):
     
     return(encrypted_msg)
 
-user_input = input("enter a message:\n > ")
+def decrypte(encrypted_msg):
 
-print(main(user_input))
+    while True:
+        try:
+            key = int(input('enter a key (0 to 26):\n > '))
+            if key >= 0 and key <= 26:
+                break
+            else:
+                print("unvalid key!")
+                continue
+        except:
+            print("unvalid key!")
+            
+    msg_len = len(encrypted_msg)
+    decrypted_msg = ""
+    ascii_calc = 0
+    i = 0 
+    
+    while i < msg_len and encrypted_msg[i] != '\0':
+        
+        if encrypted_msg[i] >= 'A' and encrypted_msg[i] <= 'Z':
+            
+            ascii_calc = upper_alpha.index(encrypted_msg[i]) - key
+            
+            if ascii_calc < 0:
+                ascii_calc += 26
+            decrypted_msg += upper_alpha[ascii_calc]
+
+        elif encrypted_msg[i] >= 'a' and encrypted_msg[i] <= 'z':
+            
+            ascii_calc = lower_alpha.index(encrypted_msg[i]) - key
+            
+            if ascii_calc < 0:
+                ascii_calc += 26
+            decrypted_msg += lower_alpha[ascii_calc]
+           
+        else:
+            decrypted_msg += encrypted_msg[i]
+            
+        i += 1
+    
+    return (decrypted_msg)
+
+
+def user_input():
+    user_deci = input('Do you want to (e)ncrypt or (d)ecrypt?\n > ')
+
+    if user_deci.lower() == 'e':
+        user_input = input("enter the message to encrypte:\n > ")
+        print(encrypte(user_input))
+    elif user_deci.lower() == 'd':
+        user_input = input("enter the message to decrypte:\n > ")
+        print(decrypte(user_input))
+        
+user_input()
